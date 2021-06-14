65e9e7ea1db47b789fa8e220d99fe5a98ba0eccd
ElideZavala <zavalavinagreelide@gmail.com>
2021-06-14 11:04:58 -0600

    maqauetación

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const api = 'api_key=1128ecbff69b87398bf97daf1d7d2927&page=1';
-const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&${api}`;
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&${api}&language=es`;
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/';
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?${api}&
-query="`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?${api}&query="`;
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
@@ -21,8 +20,45 @@ async function getMovies(url) {
 
 function showMovies(movies) {
 	main.innerHTML = '';
+
+	movies.forEach(movie => {
+		const {
+			title,
+			poster_path,
+			vote_average,
+			overview
+		} = movie;
+
+		const movieEl = document.createElement('div');
+
+		movieEl.classList.add('movie');
+
+		movieEl.innerHTML = `
+			<img src="${IMG_PATH + poster_path}" alt="${title}">
+			<div class="movie-info">
+				<h3>${title}</h3>
+				<span class="${getClassByRate(vote_average)}">${vote_average}</span>
+			</div>
+			<div class="overview">
+				<h3>Overciew</h3>
+				${overview}
+			</div>
+		`
+
+		main.appendChild(movieEl);
+	});
 }
-search
+
+function getClassByRate(vote) {
+	if (vote >= 8) {
+		return 'green';
+	} else if (vote >= 5) {
+		return 'orange';
+	} else {
+		return 'red';
+	}
+}
+
 form.addEventListener('submit', e => {
 	e.preventDefault();
 
